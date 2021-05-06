4e9493962f33e77e78edf18f7eaf738477b4d110
Paul Hazelton <hazeltonspaul@gmail.com>
2021-05-06 02:46:02 -0400

    Added navbar and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div id='App'>
+			<NavBar />
+			<div id='center-pane'>
+				<div>
+					This site is under construction!
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function NavBar() {
+	return (
+		<header>
+			<img className='logo' src='/face.svg' alt='logo' />
+			<nav>
+				<ul id='nav-link-list'>
+					<li>About Me</li>
+					<li>Contact Me</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default App;
