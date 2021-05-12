99d9a01849e009c3e83ef01bce91918739a33fbe
Paul Hazelton <hazeltonspaul@gmail.com>
2021-05-12 01:34:05 -0400

    Added ata project with img and button.

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,11 @@ function App() {
 			<NavBar />
 			<div id='center-pane1'>
 				<div id='center-pane2'>
+					<h1>What's up!?</h1><hr></hr>
 					<Welcome />
 					<ConnectList />
+					<h1>My Projects</h1><hr></hr>
+					<ProjectList />
 				</div>
 			</div>
 		</div>
@@ -31,7 +34,7 @@ function NavBar() {
 
 function Welcome() {
 	return (
-		<div id='welcome-box'>
+		<div className='info-box'>
 			<h2>Who am I?</h2>
 			<p>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim totam commodi adipisci temporibus illo quo cupiditate. Esse, perferendis explicabo sunt, officiis quia a temporibus facilis molestiae dolorem obcaecati mollitia ea.
@@ -43,23 +46,23 @@ function Welcome() {
 
 function ConnectList() {
 	return (
-		<div id='connection-box'>
+		<div className='info-box'>
 			<h2>Other Places You Can Find Me</h2>
 			<div id='connection-list'>
 				<Connection
-					link={'https://github.com/PaulHazelton'}
+					url={'https://github.com/PaulHazelton'}
 					imgPath={'/images/github-light-120.png'}
 					alt={'github'}
 					label={'GitHub'}
 				/>
 				<Connection
-					link={'https://www.linkedin.com/in/paul-hazelton/'}
+					url={'https://www.linkedin.com/in/paul-hazelton/'}
 					imgPath={'/images/linked-in-circle.png'}
 					alt={'LinkedIn'}
 					label={'LinkedIn'}
 				/>
 				<Connection
-					link={'https://openprocessing.org/user/103154?view=sketches'}
+					url={'https://openprocessing.org/user/103154?view=sketches'}
 					imgPath={'/images/openprocessing-logo.png'}
 					alt={'Open Processing'}
 					label={'Open Processing'}
@@ -70,7 +73,7 @@ function ConnectList() {
 }
 
 interface ConnectionProps {
-	link: string,
+	url: string,
 	imgPath: string,
 	alt: string,
 	label: string,
@@ -78,11 +81,34 @@ interface ConnectionProps {
 function Connection(props: ConnectionProps) {
 	return (
 		<div className='connection'>
-			<a href={props.link} target='_blank' rel='noopener noreferrer'>
+			<ExtLink url={props.url}>
 				<img className='connection-pic' src={props.imgPath} alt={props.alt} />
-			</a>
+			</ExtLink>
 			<h3 className='connection-label'>{props.label}</h3>
 		</div>
+	);
+}
+
+function ProjectList() {
+	return (
+		<div className='info-box' id='project-box'>
+			<div className='project-info'>
+				<h2>All Things Auctionable</h2>
+				<p>
+					Auction software for the non-profit organization <ExtLink url='https://www.allthingspossible.org/'>All Things Possible</ExtLink>. This was a Winthrop University team project, but now it is being finished and maintained by myself and my friend Michael Hazell.
+
+					You can view the site in action on the development server at <ExtLink url='https://allthingsauctionable.ml'>allthingsauctionable.ml</ExtLink>
+				</p>
+				<button className='button'><span>View on GitHub</span></button>
+			</div>
+			<div className='project-img' style={{backgroundImage: 'url(images/ata-screenshot.png)'}} />
+		</div>
+	);
+}
+
+function ExtLink(props: {url: string, children?: React.ReactNode}) {
+	return (
+		<a href={props.url} target='_blank' rel='noopener noreferrer'>{props.children}</a>
 	);
 }
 
