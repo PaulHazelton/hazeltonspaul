dfc402e889af16ba14f2bc98e676a78974110f10
Paul Hazelton <hazeltonspaul@gmail.com>
2021-05-24 15:49:44 -0400

    Additions to about page

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -1,4 +1,4 @@
-import { Heading } from '../common/components'
+import { Heading, ExtLink } from '../common/components'
 
 function About() {
 	return (
@@ -7,22 +7,34 @@ function About() {
 			<Heading text='About Me'/>
 			<div className='info-box' id='project-box'>
 				<div className='project-info'>
-					<h2>Who Am I?</h2>
+					<h2>Who Am I?🤔</h2>
 					<p>
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, vel quasi nostrum odit voluptatem deserunt quia aut distinctio molestiae tempora natus iusto beatae, officia reprehenderit illo? Reprehenderit deserunt numquam ipsa!
 					</p>
-					<h2>My Education</h2>
-					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam ducimus quis modi maxime doloremque ex consequatur fugiat esse tempore eos sequi, expedita velit excepturi, perspiciatis maiores vero totam voluptatibus. Laborum.
-					</p>
-					<h2>What Am I Passionate About?</h2>
+				</div>
+				<div className='project-img-box'>
+					<img src='images/HeadShot-crop.JPG' alt='My professional headshot' />
+				</div>
+			</div>
+			<div className='info-box' id='project-box'>
+				<div className='project-info'>
+					<h2>My Education🎓🎓</h2>
 					<p>
 						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam ducimus quis modi maxime doloremque ex consequatur fugiat esse tempore eos sequi, expedita velit excepturi, perspiciatis maiores vero totam voluptatibus. Laborum.
 					</p>
 				</div>
 				<div className='project-img-box'>
-					<img src='images/HeadShot.JPG' alt='TODO' />
-				</div>
+					<img src='images/winthrop.jpg' alt='Winthrop University' />
+				</div>					
+			</div>
+			<div className='info-box'>
+				<h2>What Am I Passionate About?🎮</h2>
+				<p>
+					I truly love programming, especially when the result is something visually interesting. Video games are especially fun to work on, It's very satisfying to see illustrations that I've created come to life in a game.
+				</p>
+				<p>
+					As a game designer, my mission is to develop video games that are genuinely fun to play and interesting to look at, but accessable to people without access to expensive and powerful machines. To this day, I still can't afford to purchase or build a big beutiful gaming rig, and so I simply can't play many of the games I want to, so of course the games I like to play less graphically intense one's like <ExtLink url='https://www.factorio.com/'>Factorio</ExtLink>, <ExtLink url='https://www.stardewvalley.net/'>Stardew Valley</ExtLink>, and <ExtLink url='https://www.minecraft.net/en-us'>Minecraft</ExtLink>.
+				</p>
 			</div>
 		</div>
 		<div className='section'>
