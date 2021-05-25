244e207e811153c17d45b444845e987286a3c8a3
Paul Hazelton <hazeltonspaul@gmail.com>
2021-05-25 18:35:06 -0400

    Simple download link

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,27 +1,11 @@
-import { Heading } from "../common/components";
+import { Heading, ExtLink } from "../common/components";
 
 function Resume () {
-
-	// const getData = (url: string) => new Promise((resolve, reject) => {
-	// 	setTimeout(() => {
-	// 		fetch(url)
-	// 			.then(response => response.text())
-	// 			.then(data => {
-	// 				resolve(data)
-	// 			});
-	// 	});
-	// });
-
 	return  (
 		<>
 			<div className='section' id='welcome-section'>
 				<Heading text='My Resume' />
-				{/* <DownloadLink
-					label='Download'
-					filename='Paul-Hazelton-Resume.pdf'
-					exportFile={() => Promise.resolve(getData('Paul-Hazelton-Resume.pdf'))}
-				/> */}
-				{/* <a href=''>Download from github</a> */}
+				<ExtLink url='https://raw.githubusercontent.com/PaulHazelton/hazeltonspaul/master/public/Paul-Hazelton-Resume.pdf'>Download from github</ExtLink>
 			</div>
 			<div className='section'>
 				<div id='welcome-box'>
